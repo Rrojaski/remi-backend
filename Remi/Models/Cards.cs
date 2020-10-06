9b3ca26dca93f476ea8d30e3903a1ada4919e927
R <roman.rojas@suddath.com>
2020-10-06 15:32:25 -0400

    updated cards context

--- Remi/Models/Cards.cs
+++ Remi/Models/Cards.cs
@@ -5,7 +5,7 @@ namespace Cards
     public class Card
     {
         public long Id { get; set; }
-        public String Character { get; set; }
+        public String ChineseCharacter { get; set; }
         public String Meaning { get; set; }
         public String Piyin { get; set; }
     }
